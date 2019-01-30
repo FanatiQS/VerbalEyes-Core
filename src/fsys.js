da8bdd5959b883d1e69a36e1ddaa72c13a00fdca
FanatiQS <fanatiqs@FanatiQS-MacBook-Pro.local>
2019-01-31 00:47:01 +0100

    Added system to get config object

---
+++ src/fsys.js
@@ -0,0 +1,246 @@
+'use strict';
+
+const fs = require('fs');
+
+const log = require('./log');
+
+
+
+// Add file extension 'type' to 'path'
+function addFileExt(path, type) {
+	// Get file and index of extension separator
+	const file = path.slice(path.lastIndexOf('/') + 1);
+	const dot = file.lastIndexOf('.');
+
+	// Add 'type' to end of 'path' if it has no file extension
+	if (dot <= 0) return path + type;
+
+	// Return 'path' if it has the correct file extension
+	if (file.slice(dot) === type) return path;
+
+	// Throw error if 'path' has the wrong file extension
+	const err = Error("Wrong file extension for '" + path + "', it needs to be a " + type + " file");
+	err.code = 'wrongtype';
+	throw err;
+}
+
+
+
+// Create a file and its entire path containing 'content'
+function createFile(path, content, callback) {
+	prepLoop((callback) => {
+		fs.writeFile(path, content, callback);
+	}, callback);
+}
+
+// Create directory and its entire path
+function createDir(path, callback) {
+	prepLoop((callback) => {
+		fs.mkdir(path, callback);
+	}, callback);
+}
+
+// Create setup for 'loop' function
+function prepLoop(action, callback) {
+	loop([callback, action], 1);
+}
+
+// Create entire path for function in 'callbacks'
+function loop(callbacks, i) {
+	callbacks[i]((err) => {
+		// If previous action was successfull
+		if (!err) {
+			// Decreace index
+			i--;
+
+			// Call last callback without error argument if end is reached
+			if (i === 0) {
+				callbacks[0](null);
+				return;
+			}
+		}
+		// Add callback that creates parent directory if path was not found
+		else if (err.code === 'ENOENT') {
+			callbacks.push((callback) => {
+				fs.mkdir(err.path.slice(0, err.path.lastIndexOf('/')) || '/', callback);
+			});
+			i++;
+		}
+		// Run first callback with error object and stop for other errors
+		else {
+			callbacks[0](err);
+			return;
+		}
+
+		// Run next callback from 'callbacks' array
+		loop(callbacks, i);
+	});
+}
+
+
+
+//!! Watch file for changes
+const watchers = {};
+function watchFile(path, callback) {
+	// Get name of file and path to parent directory of 'path'
+	const split = path.lastIndexOf('/');
+	const parent = (split === -1) ? '.' : path.slice(0, split) || '/';
+	const target = path.slice(split + 1);
+
+	// Create new watcher if one does not exist for the parent already
+	if (!watchers.hasOwnProperty(parent)) {
+		// Create watcher
+		watchers[parent] = fs.watch(parent, (type, name) => {
+			console.log('im in fsys checking for watched files', name);//!!
+			/*//!!## problems whith multiple calls on single change
+			//!!
+			if (!this.callbacks.hasOwnProperty(name) || this.callbacks[name].hold) return;
+
+			//!!
+			this.callbacks[name].hold = setTimeout(function () {
+				//!!
+				getFile(name, watchers[parent].callbacks[name]);
+
+				//!!
+				watchers[parent].callbacks[name].hold = null;
+			}, 100);
+
+			return;*/
+			// If modified file has callback, get content and run callback
+			/*if (this.callbacks.cooldown) return
+			this.callbacks.cooldown = setTimeout(function () {
+				watchers[parent].callbacks.cooldown = false;
+			}, 4000);
+			console.log(this.callbacks.cooldown);*/
+			//!!## 'this' does not work anymore since I changed it to an arrow function
+
+			// Run callback with file content if 'name' has a callback in this watcher
+			if (callbacks.hasOwnProperty(name)) fs.readFile(path, 'utf-8', (err, content) => {
+				// Run callback unless 'target' was renamed from 'path' to something else
+				if (!err || err.code !== 'ENOENT') callbacks[name](err, content);
+			});
+		});
+
+		// Create callbacks container
+		watchers[parent].callbacks = {};
+	}
+
+	// Get callbacks folder
+	const callbacks = watchers[parent].callbacks;
+
+	// Add callback to watcher
+	callbacks[target] = callback;
+
+	// Return path and function to stop watching target
+	return {
+		path: path,
+		close: function () {
+			// Delete callback for target
+			delete callbacks[target];
+
+			// Close and remove watcher if no targets are being watched anymore
+			if (!Object.keys(callbacks).length) {
+				watchers[parent].close();
+				delete watchers[parent];
+			}
+		}
+	};
+}
+
+// Setup watcher for JSON file
+function watchJSON(path, oldJSON) {
+	// Setup watcher for JSON file
+	return watchFile(path, (err, content) => {
+		// Error handling for errors
+		if (err) {
+			log.err("Error reading contents of watched JSON file").ERROR(err);
+			return;
+		}
+
+		// Parse content from file watching
+		try {
+			var newJSON = JSON.parse(content);
+		}
+		// Error handling for if parsing failed
+		catch (err) {
+			err.content = content;
+			log.err("Unable to parse '" + path + "'").ERROR(err);
+			return;
+		}
+
+		// Delete or update properties from 'newJSON' in 'oldJSON'
+		Object.keys(oldJSON).forEach((key) => {
+			// Ignore properties starting with underscore
+			if (key[0] === '_') return;
+
+			// Handle properties not matching
+			if (oldJSON[key] !== newJSON[key]) {
+				const pd = Object.getOwnPropertyDescriptor(oldJSON, key);
+				const hasValue = newJSON.hasOwnProperty(key);
+
+				// Get value of JSON before changing it
+				const prev = oldJSON[key];
+
+				// Handle updates if current property is configurable
+				if (pd.configurable) {
+					// Update property if 'newJSON' property exists
+					if (hasValue) {
+						try {
+							oldJSON[key] = newJSON[key];
+							log("Updated JSON property '" + key + "' from '" + prev + "' to '" + oldJSON[key] + "' in:", path);
+						}
+						// Catch and log error if updating property fails
+						catch (err) {
+							log.err("Unable to update property '" + key + "' in:", path).ERROR(err);
+						}
+					}
+					// Delete or hide property if 'newJSON' does not contain that property
+					else {
+						// Delete property if it is a normal value
+						if (pd.value) {
+							delete oldJSON[key];
+						}
+						// Hide property if it is a getter/setter
+						else {
+							Object.defineProperty(oldJSON, key, {enumerable: false});
+						}
+						log("Removed JSON property '" + key + "' with value '" + prev + "' from:", path);
+					}
+				}
+				// Error message for locked property not being updated
+				else if (hasValue) {
+					log.err("Unable to update locked property '" + key + "' in:", path);
+				}
+			}
+
+			// Remove from list of properties to add
+			delete newJSON[key];
+		});
+
+		// Add remaining properties to 'oldJSON'
+		Object.keys(newJSON).forEach((key) => {
+			// Ignore properties starting with underscore
+			if (key[0] === '_') return;
+
+			// Add new property
+			try {
+				oldJSON[key] = newJSON[key];
+				log("Added JSON property '" + key + "' with value '" + oldJSON[key] + "' to:", path);
+			}
+			// Catch and log error if updating property fails
+			catch (err) {
+				log.err("Unable to add property '" + key + "' to:", path).ERROR(err);
+			}
+		});
+	});
+}
+
+// Export modules
+module.exports = {
+	addFileExt,
+	createFile,
+	createDir,
+	watchFile,
+	watchJSON,
+	watchers
+};
---
+++ src/getConf.js
@@ -0,0 +1,173 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const log = require('./log');
+const fsys = require('./fsys');
+const observer = require('./observer');
+
+
+
+// Watch config file
+function watchConf(config, confInput) {
+	// Watch file at path 'confInput'
+	config._watcher = fsys.watchJSON(confInput, config);
+
+	// Log, set up config watcher
+	log("Watching config file:", /@1/, confInput);
+}
+
+// Get config data
+function getObj(confInput1) {
+	// Get config settings and get 'watcher' for 'config' file
+	if (typeof confInput1 === 'string') {
+		// Clarify path for console messages
+		confInput1 = path.resolve(confInput1);
+
+		// Add file extension if nonexistent
+		confInput1 = fsys.addFileExt(confInput1, '.json');
+
+		try {
+			// Get parsed JSON content of config file at path 'confInput1'
+			const config = JSON.parse(fs.readFileSync(confInput1, 'utf-8'));
+
+			// Log, completed config setup
+			log("Successfully read config file:", /@1/, confInput1);
+
+			// Watch 'config'
+			watchConf(config, confInput1);
+
+			// Return config object
+			return config;
+		} catch (err) {
+			// Create new config file if it was not found
+			if (err.code === 'ENOENT') {
+				// Log, creating new file, it does not exist
+				log.err("Unable to locate config file. Creating a new one:", confInput1);
+
+				// Create new config object
+				const config = {_createFileCallback: null};
+
+				// Create a new file and entire path asynchronously
+				fsys.createFile(confInput1, '{\n\t\n}', (err) => {
+					// Get '_createFileCallback' callback function
+					const callback = config._createFileCallback;
+
+					// Delete '_createFileCallback' for cleaner look in callback response
+					delete config._createFileCallback;
+
+					// Fire 'callback' if it exists and return
+					if (callback) {
+						callback(err, confInput1);
+						if (err) return;
+					}
+
+					// Log and throw error if no error catcher function exists
+					if (err) {
+						log.err("Failed to create config file at:", confInput1);
+						throw err;
+					}
+
+					// Log, created new file
+					log("Created new config file at:", confInput1);
+
+					// Watch 'config'
+					watchConf(config, confInput1);
+				});
+
+				// Return config object
+				return config;
+			}
+			// Error handling for errors other than if file is not found
+			else {
+				log.err((err.message.match(/JSON/)) ? "Error parsing config file:" : "Error getting config file:", confInput1);
+				throw err;
+			}
+		}
+	}
+	// Return input object
+	else if (confInput1 instanceof Object) {
+		return confInput1;
+	}
+	// Error handling for unsupported argument types
+	else {
+		throw TypeError("Unable to setup config, 'confInput1' needs to be an object or a string path to a JSON file");
+	}
+}
+
+// Get config object, create middlewares and links
+module.exports = function (confInput1, confInput2, observers) {
+	// Log, getting config settings
+	log("\nSetting up config settings...");
+
+	// Set 'confInput1' to './config.json' if it is left undefined
+	if (!confInput1) confInput1 = './config.json';
+
+	// Get conf data
+	const conf = getObj(confInput1);
+
+	// Set up observer for every property in 'observers' on 'conf' and fire observers callback on change
+	Object.keys(observers).forEach((key) => {
+		const value = observers[key];
+		if (typeof value !== 'function') throw Error("Observer callback for '" + key + "' has to be a function: " + value);
+		observer(conf, key, value);
+	});
+
+	// Add links to all 'confInput2' properties in 'conf'
+	if (confInput2) {
+		const locked = Object.keys(confInput2);
+		locked.forEach((key) => {
+			// Log, error message for if 'conf' already has the property
+			if (conf.propertyIsEnumerable(key)) log.err("Unable to use '" + key + "' from config, property is locked");
+
+			// Update 'confInput2' property if 'conf' has setter middleware
+			const mid = ((Object.getOwnPropertyDescriptor(conf, key) || {}).set || {}).mid;
+			if (mid) {
+				// Get property descriptor of 'confInput2'
+				const conf2Prop = Object.getOwnPropertyDescriptor(confInput2, key);
+
+				// Create getter/setter if value is used
+				if (conf2Prop.value) {
+					conf2Prop.get = function () {
+						return conf2Prop.value;
+					};
+					conf2Prop.set = function (value) {
+						conf2Prop.value = value;
+					};
+				}
+
+				// Set up getter/setter on 'confInput2'
+				Object.defineProperty(confInput2, key, {
+					get: conf2Prop.get,
+					set: (!conf2Prop.set) ? undefined : function (value) {
+						mid(value, this[key]);
+						conf2Prop.set(value);
+					}
+				});
+			}
+
+			// Create link to 'confInput2' property in 'conf'
+			Object.defineProperty(conf, key, {
+				get: function () {
+					return confInput2[key];
+				},
+				set: function () {
+					log.err("Unable to update '" + key + "' in config, property is locked");//!!## try to find another way of preventing crahses
+				},
+				enumerable: true,
+				configurable: false
+			});
+		});
+
+		// Log, locked properties list
+		log("Locked properties in config:\n\t" + locked.join('\n\t'));
+	}
+
+	// Log, finnished setting up config with content of 'conf' unless it is empty
+	const confKeys = Object.keys(conf);
+	log("Completed config setup" + (!confKeys.length ? '' : " with settings:" + confKeys.map((key) => (key[0] === '_') ? null : '\n\t' + key + ': ' + conf[key]).join('')));
+
+	// Return 'conf' object
+	return conf;
+};
