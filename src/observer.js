83280b8f8136676088323a1ff1faf41d0d99bf53
FanatiQS <fanatiqs@FanatiQS-MBP.lan>
2019-06-19 12:41:26 +0200

    Refactor

--- src/observer.js
+++ src/observer.js
@@ -3,26 +3,26 @@
 // Add middleware to objects property to run callback when value is updated
 module.exports = function observer(obj, key, callback) {
 	// Get original/current properties
-	const prop = Object.getOwnPropertyDescriptor(obj, key) || {};
+	const descriptor = Object.getOwnPropertyDescriptor(obj, key) || {};
 
 	// Create getter and setter linked to value if no getter or setter already exist
-	if (!(prop.get || prop.set)) {
-		prop.get = function () {
-			return prop.value;
+	if (!(descriptor.get || descriptor.set)) {
+		descriptor.get = function () {
+			return descriptor.value;
 		};
-		prop.set = function (value) {
-			prop.value = value;
+		descriptor.set = function (value) {
+			descriptor.value = value;
 		};
 	}
 
 	// Create setter function unless 'obj' is getter only
-	if (prop.set) {
+	if (descriptor.set) {
 		var setter = function (value) {
 			// Run middleware with new and old property value
 			setter.mid(value, this[key]);
 
 			// Run setter function
-			prop.set(value);
+			descriptor.set(value);
 		};
 
 		// Create middleware function
@@ -41,7 +41,7 @@ module.exports = function observer(obj, key, callback) {
 
 	// Set property to use getter/setter
 	Object.defineProperty(obj, key, {
-		get: prop.get,
+		get: descriptor.get,
 		set: setter,
 		configurable: true
 	});
