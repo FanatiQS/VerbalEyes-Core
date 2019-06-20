8eeaf1b3bb6f5ef7e14f45a656be387fc1ec8090
FanatiQS <fanatiqs@FanatiQS-MBP.lan>
2019-06-20 11:51:24 +0200

    Added system to observe changes to all child properties

--- src/observer.js
+++ src/observer.js
@@ -1,9 +1,12 @@
 'use strict';
 
-// Add middleware to objects property to run callback when value is updated
+// Run callback on update to objects property
 exports.observe = function observe(obj, key, callback) {
 	// Get original/current properties
 	const descriptor = Object.getOwnPropertyDescriptor(obj, key) || {};
+
+	// Abort if a middleware already exists
+	if (descriptor.set && descriptor.set.mid) return;
 
 	// Create getter and setter linked to value if no getter or setter already exist
 	if (!(descriptor.get || descriptor.set)) {
@@ -46,3 +49,51 @@ exports.observe = function observe(obj, key, callback) {
 		configurable: true
 	});
 };
+
+// Run callback on update to objects property and child properties
+exports.observeAll = function (obj, key, callback) {
+	let wait = false;
+	let cleanup = deepObserve(obj, key, (value) => {
+		// Only run callback once
+		if (wait) return;
+		wait = true;
+
+		// Run callback when all changes to 'obj' has been made
+		process.nextTick(() => {
+			cleanup();
+			callback(value);
+			wait = false;
+		});
+	})
+}
+
+// Observe all child properties of property 'key' in 'obj'
+function deepObserve(obj, key, callback) {
+	let stored;
+
+	// Observe 'key' property on 'obj' for changes
+	exports.observe(obj, key, callback);
+
+	// Observe all immediate child properties
+	if (typeof obj[key] === 'object') {
+		var cleanupers = Object.keys(obj[key]).map((key2) => {
+			return deepObserve(obj[key], key2, (value) => {
+				// Add 'value' to 'key2' property in predefined object
+				if (stored) {
+					stored[key2] = value;
+					return;
+				}
+
+				// Create new object for 'value' at 'key2' and return parent
+				stored = {[key2]: value};
+				callback(stored);
+			})
+		})
+	}
+
+	// Return function to empty all child 'stored' variables
+	return () => {
+		stored = null;
+		if (cleanupers) cleanupers.forEach((empty) => empty());
+	};
+}
